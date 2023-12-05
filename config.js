a592e3a8913877b8bd011f1cb18a8117ff799ea9
officialwainaina <152956773+officialwainaina@users.noreply.github.com>
2023-12-05 10:17:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254741789111', 'Wainaina Official Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
